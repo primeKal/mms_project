6af7637c67161ff5e11d6529fdb749667218cfd2
Frezer <frezerzewdu@gmail.com>
2023-10-20 23:07:16 +0400

    main view constructed

---
+++ mms_client/src/views/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="grid grid-cols-6">
+        <NavBarVue class="col-span-1 min-h-screen shadow-lg"/>
+        <div class="col-span-5 h-full">
+            <div class="flex flex-row-reverse bg-white h-9 shadow-lg z-10">
+                <p>Profile</p>
+            </div>
+            <div>
+                <router-view/>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import NavBarVue from '@/layout/NavBar.vue'
+export default {
+    components: {
+        NavBarVue,
+    }
+}
+</script>
