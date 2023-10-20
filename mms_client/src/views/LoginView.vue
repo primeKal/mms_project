9586a9f7ebbd186f525bac0bcdbb140a039aa91c
Frezer <frezerzewdu@gmail.com>
2023-10-20 23:07:05 +0400

    bug fix

--- mms_client/src/views/LoginView.vue
+++ mms_client/src/views/LoginView.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="h-screen w-full flex flex-col justify-center items-center bg-white">
         <div class="w-1/3 p-5 flex flex-col bg-slate-100 rounded-lg shadow-lg">
-            <h2 class="font-semibold text-2xl">Log In</h2>
+            <h2 class="font-semibold text-2xl bg-skin-primary">Log In</h2>
             <h6 class="font-light text-black/70">Get started</h6>
-            <div class="mt-10">
+            <div class="mt-10 flex flex-col">
                 <input class="py-2 pl-2 rounded border" placeholder="Phone number" type="text"/>
                 <input class="mt-3 py-2 pl-2 rounded border" placeholder="Password" type="password"/>
             </div>
