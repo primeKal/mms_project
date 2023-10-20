756b89981542647df970f52e603e0ed01cbecb8d
Frezer <frezerzewdu@gmail.com>
2023-10-20 22:58:07 +0400

    primary color added

--- mms_client/tailwind.config.js
+++ mms_client/tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), 1)`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#43AA84',
+      },
+      textColor: {
+        skin: {
+          primary: withOpacity("--color-primary"),
+          a11y: withOpacity("--color-a11y")
+        }
+      },
+      backgroundColor: {
+        skin: {
+          primary: withOpacity("--primary-bg-color"),
+          a11y: withOpacity("--secondary-bg-color"),
+        },
+       
+      }
+    },
   },
   plugins: [],
 }
