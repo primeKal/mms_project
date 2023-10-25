9e6c8ee23243250bab6c45eb5374a5a1827fa224
Frezer <frezerzewdu@gmail.com>
2023-10-25 10:18:53 +0400

    all components added

--- mms_client/src/views/MenuMangementView/MenuManagementIndex.vue
+++ mms_client/src/views/MenuMangementView/MenuManagementIndex.vue
@@ -2,22 +2,24 @@
     <div class="p-6 pr-12 w-full">
         <h3 class="text-primary font-medium text-xl">Category</h3>
         <div class="mt-4 w-full flex justify-between">
-            <div class="relative h-min w-2/3 ">
-                <input class="w-full py-2 pl-12 text-sm font-light border rounded" placeholder="Search Category" />
-                <svg class="absolute left-4 top-2 text-black/60" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z"/></svg>
-            </div>
+           <SearchBarVue 
+                class="w-2/3"
+                :placeholderString="'Search categories'" 
+                @searchChanged="searchCategories"
+           />
             <button class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
                 + NEW CATEGORY
             </button>
         </div>
         <CategoriesTableVue class="mt-4" :categories="menuCategories"/>
-        <h3 class="mt-10 text-primary font-medium text-xl">Category</h3>
+        <h3 class="mt-10 text-primary font-medium text-xl">Menu List</h3>
         <div class="mt-4 w-full flex justify-between">
             <div class="flex justify-between w-3/5">
-                <div class="relative w-3/5">
-                    <input class="w-full py-2 pl-12 text-sm font-light border rounded" placeholder="Search Category" />
-                    <svg class="absolute left-4 top-2 text-black/60" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z"/></svg>
-                </div>
+                <SearchBarVue 
+                    class="w-3/5"
+                    :placeholderString="'Search menus'"
+                    @searchChanged="searchMenuItem"
+                />
                 <fieldset class="-mt-1 w-1/3 border rounded">
                     <legend class="ml-3 text-xs px-1">Category</legend>
                     <select class="w-full pt-1 pb-1 px-2 outline-none">
@@ -31,14 +33,18 @@
                 + NEW MENU
             </button>
         </div>
-        
+        <MenuListTableVue class="mt-5" />
     </div>
 </template>
 <script>
+import SearchBarVue from '@/components/fields/SearchBar.vue'
 import CategoriesTableVue from './CategoriesTable.vue'
+import MenuListTableVue from './MenuListTable.vue'
 export default {
     components: {
+        SearchBarVue,
         CategoriesTableVue,
+        MenuListTableVue,
     },
     data () {
         return {
@@ -48,6 +54,15 @@ export default {
                 {'id': 3, 'name': 'Non-fasting', 'description': 'Lorem ipsum dollar sit...'},
                 {'id': 4, 'name': 'Drinks', 'description': 'Lorem ipsum dollar sit...'},
             ]
+        }
+    },
+    methods: {
+        searchCategories(searchString) {
+            console.log(searchString)
+            // do the search
+        },
+        searchMenuItem (searchString) {
+            console.log(searchString)
         }
     }
 }
