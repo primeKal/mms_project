76c9bf12c16dd8237151a7b96100314e789adf45
Frezer <frezerzewdu@gmail.com>
2023-11-02 14:52:08 +0400

    css themes commented

--- mms_client/src/App.vue
+++ mms_client/src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <router-view/>
 </template>
-
-<style>
+<!-- <script>
+export default {
+  data () {
+    return {
+      primaryColor: '67,170,132',
+      secondayColor: '#2F80ED'
+    }
+  },
+  created () {
+    // api call
+    // this.primaryColor = getRGBColor("#43AA84", "primary")
+  }
+}
+</script>
+<script setup>
+  // const secondaryColor = '67'
+</script>
+<style scoped>
+:root {
+  --primary-bg-color: '255,255,0';
+  --secondary-bg-color: v-bind('secondayColor');
+}
+</style> -->
