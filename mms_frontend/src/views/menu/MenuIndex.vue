38bb947692b69501c6adb152cd53dd635f81312b
Frezer <frezerzewdu@gmail.com>
2023-11-07 09:30:45 +0400

    structure changed

--- mms_frontend/src/views/menu/MenuIndex.vue
+++ mms_frontend/src/views/menu/MenuIndex.vue
@@ -1,21 +1,28 @@
 <template>
-    <div class="h-full p-7 bg-gradient-to-b from-emerald-100 to-emerald-200 text-sm">
-        <div class="p-10 h-5/6 flex flex-col bg-white shadow-lg rounded-lg">
-            <div class="flex justify-between">
-                <select class="w-1/5 py-2 px-3 border rounded " placeholder="Select Menu">
-                    <option disabled selected class="text-black/70">Select Menu</option>
-                    <option>Primary Menu</option>
-                </select>
-                <button class="py-2 px-4 primary-button">
-                    + Add Menu
-                </button>
-            </div>
-        </div>
+    <div class="h-full p-7 flex flex-col bg-gradient-to-b from-emerald-100 to-emerald-200">
+        <img src="/images/menu_image.png" class="self-center h-32 w-32 object-cover object-center rounded-full" />
+        <h2 class="text-center mt-2 text-primary">Sheraton Addis</h2>
+        <router-view v-slot="{Component}">
+            <!-- enter all transitions -->
+            <transition 
+                mode="out-in"   
+            >
+                <component :is="Component"></component>
+            </transition>
+        </router-view>  
+
     </div>
 </template>
 <script>
+import { useRoute } from 'vue-router'
 export default {
+    setup () {
+        const route = useRoute();
+        console.log(route.query)
+        // fetch menu information from API
+    },
     mounted () {
+        // get menu id from url
         this.$emit('select_nav', 1)
     }
 }
