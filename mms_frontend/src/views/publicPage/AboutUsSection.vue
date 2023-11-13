5959db7a92d571255137b1a8683b6a4da3fbb131
Frezer <frezerzewdu@gmail.com>
2023-11-13 18:30:06 +0400

    other public pages initiated

---
+++ mms_frontend/src/views/publicPage/AboutUsSection.vue
@@ -0,0 +1,43 @@
+<template>
+    <section class="py-20 bg-primary text-white flex justify-around">
+        <div class="basis-2/5">
+            <p class="text-5xl">How We Support Restaurants <br/> with Our QR Code Ordering <br/> System</p>
+            <p class="mt-14">
+                Experience comprehensive support for your restaurant globally. Our QR Code Ordering <br/>
+                System revolutionizes efficiency, safety, and growth for dining establishments worldwide.
+            </p>
+        </div>
+        <div class="basis-2/5 flex flex-col space-y-8">
+            <div class="flex items-start">
+                <div class="p-4 bg-white text-primary">
+                    <!-- line svg -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m3.5 18.5l6-6l4 4L22 6.92L20.59 5.5l-7.09 8l-4-4L2 17l1.5 1.5Z"/></svg>
+                </div>
+                <div class="ml-4">
+                    <p class="text-xl">Simplify Orders, Elevate Service</p>
+                    <p class="mt-4">Seamless order processing for restaurants, boosting efficiency and customer satisfaction.</p>
+                </div>
+            </div>
+            <div class="flex items-start">
+                <div class="p-4 bg-white text-primary">
+                    <!-- pie chart svg -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32"><g fill="currentColor"><path d="M29 17.518a.5.5 0 0 0-.5-.5H15V3.5a.5.5 0 0 0-.5-.5C6.505 3 0 9.495 0 17.479C0 25.757 6.243 32 14.521 32C22.234 32 29 25.232 29 17.518zm-28-.039c0-7.266 5.787-13.206 13-13.47v13.509c0 .276.224.5.5.482h13.49c-.283 6.99-6.455 13-13.469 13C6.813 31 1 25.188 1 17.479z"/><path d="M17.5 15h13.999c.276.018.501-.224.501-.5C32 6.505 25.495 0 17.5 0a.5.5 0 0 0-.5.5v14.018c0 .276.224.5.5.482zM18 1.009c7.063.259 12.759 5.97 12.994 13.009H18V1.009z"/></g></svg>
+                </div>
+                <div class="ml-4">
+                    <p class="text-xl">Safe, Contactless Dining</p>
+                    <p class="mt-4">Enable touch-free ordering, payments, and a secure dining experience for your customers.</p>
+                </div>
+            </div>
+            <div class="flex items-start">
+                <div class="p-4 bg-white text-primary">
+                    <!-- command svg -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="M180 144h-20v-32h20a36 36 0 1 0-36-36v20h-32V76a36 36 0 1 0-36 36h20v32H76a36 36 0 1 0 36 36v-20h32v20a36 36 0 1 0 36-36Zm-20-68a20 20 0 1 1 20 20h-20ZM56 76a20 20 0 0 1 40 0v20H76a20 20 0 0 1-20-20Zm40 104a20 20 0 1 1-20-20h20Zm16-68h32v32h-32Zm68 88a20 20 0 0 1-20-20v-20h20a20 20 0 0 1 0 40Z"/></svg>
+                </div>
+                <div class="ml-4">
+                    <p class="text-xl">Drive Growth with Smart Analytics</p>
+                    <p class="mt-4">Leverage powerful analytics to gain actionable insights, boost revenue, and enhance overall restaurant performance.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
--- mms_frontend/src/views/publicPage/PublicIndex.vue
+++ mms_frontend/src/views/publicPage/PublicIndex.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
-        public page
-        <div class="mt-10 flex space-x-5">
-            <button class="py-3 px-6 bg-green-400 rounded text-white font-semibold">
-                login
-            </button>
-            <button class="py-3 px-6 bg-green-400 rounded text-white font-semibold">
-                signup
-            </button>
-            <button @click="$router.push('menu/2')" class="py-3 px-6 bg-green-400 rounded text-white font-semibold">
-                menu page
-            </button>
-        </div>
+       <NavBarVue :selectedSection="selectedSection" />
+       <LandingSectionVue />
+       <AboutUsSectionVue />
+        <PricingSectionVue />
+        <FooterLayoutVue />
     </div>
 </template>
+<script>
+import NavBarVue from '@/layout/NavBar.vue'
+import LandingSectionVue from './LandingSection.vue'
+import AboutUsSectionVue from './AboutUsSection.vue'
+import PricingSectionVue from './PricingSection.vue'
+import FooterLayoutVue from '@/layout/FooterLayout.vue'
+export default {
+    components:{
+        NavBarVue,
+        LandingSectionVue,
+        AboutUsSectionVue,
+        PricingSectionVue,
+        FooterLayoutVue,
+    },
+    data() {
+        return {
+            selectedSection: 0
+        }
+    }
+}
+</script>
