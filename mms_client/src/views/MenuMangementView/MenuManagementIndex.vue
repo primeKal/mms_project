9334a2abaa38cabb3060f854541c4b2b38174760
Frezer <frezerzewdu@gmail.com>
2023-11-13 09:17:37 +0400

    menu changes

--- mms_client/src/views/MenuMangementView/MenuManagementIndex.vue
+++ mms_client/src/views/MenuMangementView/MenuManagementIndex.vue
@@ -1,17 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-    <div class="p-6 pr-12 w-full">
-        <h3 class="text-primary font-medium text-xl">Menu List</h3>
-        <div class="mt-4 w-full flex justify-between">
-           <SearchBarVue 
-                class="w-2/3"
-                :placeholderString="'Search menus'" 
-                @searchChanged="searchCategories"
-           />
-            <button class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
-                + NEW MENU
-            </button>
-=======
     <MainRendererVue :loading="loading">
         <DeleteConfirmationModalVue 
             v-if="openDelete"
@@ -78,53 +65,23 @@
                     </tr>
                 </tbody>
             </table>
->>>>>>> Stashed changes
         </div>
-        <table class="mt-6 w-full">
-            <thead class="bg-gray-100 border-y shadow-sm">
-                <th class="py-2 pl-4 text-left">Menu No.</th>
-                <th class="py-2 pl-4 text-left">Menu Name</th>
-                <th class="py-2 pl-4 text-left">Status</th>
-                <th class="py-2 pl-4 text-center">Actions</th>
-            </thead>
-            <tbody>
-                <tr 
-                    v-for="menu in menus"
-                    :key="menu.id"
-                    class="hover:bg-slate-50/60 border-b transition-colors"
-                >
-                    <td class="pl-4 py-3 font-light">{{ menu.id }}</td>
-                    <td class="pl-4 py-3 font-light">{{ menu.name }}</td>
-                    <td class="pl-4 py-3 font-light">{{ menu.status }}</td>
-                    <td class="pl-4 py-3 flex justify-center items-center">
-                        <svg class="p-1 hover:border rounded hover:text-blue-400 cursor-pointer transition-all" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5a2 2 0 0 0-2-2zm0 16H5V7h14v12zm-7-8.5c1.84 0 3.48.96 4.34 2.5c-.86 1.54-2.5 2.5-4.34 2.5s-3.48-.96-4.34-2.5c.86-1.54 2.5-2.5 4.34-2.5M12 9c-2.73 0-5.06 1.66-6 4c.94 2.34 3.27 4 6 4s5.06-1.66 6-4c-.94-2.34-3.27-4-6-4zm0 5.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5s1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z"/></svg>
-                        <svg @click="$router.push({name: 'edit_menu', params: {name: menu.name}})" class="p-1 hover:border rounded hover:text-yellow-400 cursor-pointer transition-all" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0L5 16ZM15 6l3 3m-5 11h8"/></svg>
-                        <svg class="ml-3 p-1 hover:border rounded hover:text-red-500 cursor-pointer transition-all" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12M8 9h8v10H8V9m7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5Z"/></svg>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    </MainRendererVue>
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
 
 import MainRendererVue from '@/layout/MainRenderer.vue'
->>>>>>> Stashed changes
 import SearchBarVue from '@/components/fields/SearchBar.vue'
 import DeleteConfirmationModalVue from '@/components/modals/DeleteConfirmationModal.vue'
 export default {
     components: {
+        MainRendererVue,
         SearchBarVue,
         DeleteConfirmationModalVue,
     },
     data () {
         return {
-<<<<<<< Updated upstream
-            menus: [
-                {'id': '0001', 'name': 'primary', 'status': 'Drafted'}
-            ]
-=======
             loading: true,
             newMenu: {
                 name: '',
@@ -132,6 +89,11 @@ export default {
             openNewMenu: false,
             openDelete: null,
         }
+    },
+    computed: {
+        ...mapGetters({
+            menus: 'Menu/getAllMenus'
+        })
     },
     methods: {
         ...mapActions({
@@ -144,8 +106,6 @@ export default {
         },
         searchMenuItem (searchString) {
             console.log(searchString)
-<<<<<<< Updated upstream
-=======
         },
         async createMenu() {
             this.loading = true
@@ -175,11 +135,10 @@ export default {
             }
         }
     },
-    mounted () {
+    async mounted () {
         this.$emit('selectedNav', 3)
         await this.fetchMenus(1)
         this.loading = false
->>>>>>> Stashed changes
     }
 }
 </script>
