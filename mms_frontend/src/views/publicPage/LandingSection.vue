56370c4871d1a726ceeacfb4c7aa2eb2aa12f116
Frezer <frezerzewdu@gmail.com>
2023-11-13 18:29:16 +0400

    landing section complete

---
+++ mms_frontend/src/views/publicPage/LandingSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="h-screen w-full flex justify-center items-center">
+        <div class="flex flex-col">
+            <p class="text-6xl font-semibold">Revolutionize your Restaurant's <br/> Ordering System</p>
+            <img class="my-14" src="@/assets/images/line.png" />
+            <p class="text-lg">
+                Revolutionize Your Restaurant's Operations with Our QR Code Ordering System: <br/>
+                Simplify Orders, Payments, and Customer Engagement, All in One Solution!
+            </p>
+            <button class="mt-16 py-3 px-5 rounded-3xl bg-primary text-white flex items-center self-start" >
+                <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m-2 14.5l6-4.5l-6-4.5v9Z"/></svg>
+                View Demo
+            </button>
+        </div>
+        <img src="@/assets/images/landing_image.png" />
+    </section>
+</template>
