86cb3b7ad5febe4274d50179d2b2c3e98ed60a9f
Frezer <frezerzewdu@gmail.com>
2023-11-14 17:34:03 +0400

    order service complete

--- mms_client/src/services/order/order.js
+++ mms_client/src/services/order/order.js
@@ -1,6 +1,29 @@
+/* eslint-disable no-async-promise-executor */
 import baseAPI from "../base";
 
 class OrderAPI {
-
+    static getOrders() {
+        return new Promise(async(resolve, reject)=>{
+            try{
+                const response = await baseAPI.get('order')
+                const result = response.data
+                resolve(result)
+            }catch(error) {
+                reject(error)
+            }
+        })
+    }
+    static editOrderStatus(status)  {
+        return new Promise(async(resolve, reject)=>{
+            try {
+                const response = await baseAPI.put('order',{
+                    status: status
+                })
+                resolve(response)
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
 }
 export default OrderAPI
