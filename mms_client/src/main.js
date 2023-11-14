284d41916eb7254888b31dc21cb0cef0ef374f56
Frezer <frezerzewdu@gmail.com>
2023-11-14 09:15:32 +0400

    vue-ouside click installed

--- mms_client/src/main.js
+++ mms_client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Toaster from '@meforma/vue-toaster'
+import vClickOutside from 'click-outside-vue3'
 
 import filter from './filters/index'
 import './assets/tailwind.css'
@@ -12,5 +13,5 @@ const toasterProperties = {
 }
 
 const app = createApp(App)
-app.use(store).use(router).use(Toaster,toasterProperties).mount('#app')
+app.use(store).use(router).use(Toaster,toasterProperties).use(vClickOutside).mount('#app')
 app.config.globalProperties.$filter = filter
