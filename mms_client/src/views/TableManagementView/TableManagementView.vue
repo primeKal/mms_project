a03e2e663b2ad17b07a58551975d0911cf9cd00d
Frezer <frezerzewdu@gmail.com>
2023-11-14 17:37:01 +0400

    table loading animation added

--- mms_client/src/views/TableManagementView/TableManagementView.vue
+++ mms_client/src/views/TableManagementView/TableManagementView.vue
@@ -1,53 +1,56 @@
 <template>
-    <div class="p-6 pr-12 w-full">
-        <AddTableViewVue class="z-10" v-if="addTable" @closeModal="()=>addTable=false" />
+    <MainRendererVue :loading="loading">
+        <div class="p-6 pr-12 w-full">
+            <AddTableViewVue class="z-10" v-if="addTable" @closeModal="()=>addTable=false" />
 
-        <h3 class="text-primary font-medium text-xl">Table</h3>
-        <div class="mt-8 w-full flex justify-between">
-            <div class="flex justify-between w-3/5">
-                <SearchBarVue 
-                    class="w-3/5"
-                    :placeholderString="'Search by table number'"
-                    @searchChanged="searchTables"
-                />
-                <fieldset class="-mt-1 w-1/3 border rounded">
-                    <legend class="ml-3 text-xs px-1">Table Status</legend>
-                    <select class="w-full pt-1 pb-1 px-2 outline-none">
-                        <option>All</option>
-                        <option>Free</option>
-                        <option>Ocuppied</option>
-                    </select>
-                </fieldset>
+            <h3 class="text-primary font-medium text-xl">Table</h3>
+            <div class="mt-8 w-full flex justify-between">
+                <div class="flex justify-between w-3/5">
+                    <SearchBarVue 
+                        class="w-3/5"
+                        :placeholderString="'Search by table number'"
+                        @searchChanged="searchTables"
+                    />
+                    <fieldset class="-mt-1 w-1/3 border rounded">
+                        <legend class="ml-3 text-xs px-1">Table Status</legend>
+                        <select class="w-full pt-1 pb-1 px-2 outline-none">
+                            <option>All</option>
+                            <option>Free</option>
+                            <option>Ocuppied</option>
+                        </select>
+                    </fieldset>
 
+                </div>
+            <button @click="addTable=true" class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
+                    + NEW TABLE
+                </button>
             </div>
-           <button @click="addTable=true" class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
-                + NEW TABLE
-            </button>
+            <TableListVue 
+                class="mt-5"
+                :tables="tables"
+            />
         </div>
-        <TableListVue 
-            class="mt-5"
-            :tables="tables"
-        />
-    </div>
+    </MainRendererVue>        
 </template>
 <script>
 import { mapGetters } from 'vuex'
 
+import MainRendererVue from '@/layout/MainRenderer.vue';
 import AddTableViewVue from './AddTableView.vue'
 import SearchBarVue from '@/components/fields/SearchBar.vue';
 import TableListVue from './TablesList.vue'
 export default {
     components: {
+        MainRendererVue,
         AddTableViewVue,
         SearchBarVue,
         TableListVue,
     },
     data () {
         return {
-            // tables: [
-            //     {'id': '001', 'size': 4, 'status': 'Occupied'}
-            // ],
+            loading: true,
             addTable: false,
+            openDeleteTable: null,
         }
     },
     computed: {
@@ -58,12 +61,12 @@ export default {
     methods: {
         searchTables(searchString) {
             console.log(searchString)
-        }
+        },
     },
     async mounted () {
         this.$emit('selectedNav', 4)
-        const status = await this.$store.dispatch('Table/fetchTables')
-        console.log(status)
+        await this.$store.dispatch('Table/fetchTables')
+        this.loading = false
     }
 }
 </script>
