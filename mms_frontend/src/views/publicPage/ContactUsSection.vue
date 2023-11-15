9faec65857c4e487fc19706b58e8eeed768a62ba
Frezer <frezerzewdu@gmail.com>
2023-11-15 19:50:00 +0400

    contact us section initalized

---
+++ mms_frontend/src/views/publicPage/ContactUsSection.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="bg-white py-20">
+        <div class="flex justify-around">
+            <div class="basis-1/3">
+                <p class="text-4xl font-medium">Get in touch with us</p>
+                <p class="mt-8">
+                    Reach out for inquiries, support, or to explore how our QR Code<br/> Ordering System can elevate your restaurant experience.
+                </p>
+                <div class="mt-14 flex flex-col space-y-8">
+                    <div class="flex items-center">
+                        <div class="p-3 bg-secondary text-white rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4c2.2 0 4 1.8 4 4c0 2.1-2.1 5.5-4 7.9c-1.9-2.5-4-5.8-4-7.9c0-2.2 1.8-4 4-4m0-2C8.7 2 6 4.7 6 8c0 4.5 6 11 6 11s6-6.6 6-11c0-3.3-2.7-6-6-6m0 4c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m8 13c0 2.2-3.6 4-8 4s-8-1.8-8-4c0-1.3 1.2-2.4 3.1-3.2l.6.9c-1 .5-1.7 1.1-1.7 1.8c0 1.4 2.7 2.5 6 2.5s6-1.1 6-2.5c0-.7-.7-1.3-1.8-1.8l.6-.9c2 .8 3.2 1.9 3.2 3.2Z"/></svg>
+                        </div>
+                        <p class="ml-3">Riverside 25, San Francisco, California</p>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="p-3 bg-secondary text-white rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6m-2 0l-8 5l-8-5h16m0 12H4V8l8 5l8-5v10Z"/></svg>
+                        </div>
+                        <p class="ml-3">qrmenu@gmail.com</p>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="p-3 bg-secondary text-white rounded-full">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M20 15.5c-1.2 0-2.5-.2-3.6-.6h-.3c-.3 0-.5.1-.7.3l-2.2 2.2c-2.8-1.5-5.2-3.8-6.6-6.6l2.2-2.2c.3-.3.4-.7.2-1c-.3-1.1-.5-2.4-.5-3.6c0-.5-.5-1-1-1H4c-.5 0-1 .5-1 1c0 9.4 7.6 17 17 17c.5 0 1-.5 1-1v-3.5c0-.5-.5-1-1-1M5 5h1.5c.1.9.3 1.8.5 2.6L5.8 8.8C5.4 7.6 5.1 6.3 5 5m14 14c-1.3-.1-2.6-.4-3.8-.8l1.2-1.2c.8.2 1.7.4 2.6.4V19Z"/></svg>
+                        </div>
+                        <p class="ml-3">+251-911-11-1121</p>
+                    </div>
+                    <ul class="w-1/2 flex justify-between">
+                        <li class="p-2 text-secondary hover:text-white bg-gray-200 hover:bg-secondary transition-colors rounded-full cursor-pointer">
+                            <!-- instagram -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/></svg>
+                        </li>
+                        <li class="p-2 text-secondary hover:text-white bg-gray-200 hover:bg-secondary transition-colors rounded-full cursor-pointer">
+                            <!-- facebook -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2.04c-5.5 0-10 4.49-10 10.02c0 5 3.66 9.15 8.44 9.9v-7H7.9v-2.9h2.54V9.85c0-2.51 1.49-3.89 3.78-3.89c1.09 0 2.23.19 2.23.19v2.47h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.45 2.9h-2.33v7a10 10 0 0 0 8.44-9.9c0-5.53-4.5-10.02-10-10.02Z"/></svg>
+                        </li>
+                        <li class="p-2 text-secondary hover:text-white bg-gray-200 hover:bg-secondary transition-colors rounded-full cursor-pointer">
+                            <!-- twitter -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23Z"/></svg>
+                        </li>
+                        <li class="p-2 text-secondary hover:text-white bg-gray-200 hover:bg-secondary transition-colors rounded-full cursor-pointer">
+                            <!-- telegram -->
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19c-.14.75-.42 1-.68 1.03c-.58.05-1.02-.38-1.58-.75c-.88-.58-1.38-.94-2.23-1.5c-.99-.65-.35-1.01.22-1.59c.15-.15 2.71-2.48 2.76-2.69a.2.2 0 0 0-.05-.18c-.06-.05-.14-.03-.21-.02c-.09.02-1.49.95-4.22 2.79c-.4.27-.76.41-1.08.4c-.36-.01-1.04-.2-1.55-.37c-.63-.2-1.12-.31-1.08-.66c.02-.18.27-.36.74-.55c2.92-1.27 4.86-2.11 5.83-2.51c2.78-1.16 3.35-1.36 3.73-1.36c.08 0 .27.02.39.12c.1.08.13.19.14.27c-.01.06.01.24 0 .38z"/></svg>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="basis-2/5">
+                <p class="border-y w-max">MAIL US</p>
+                <p class="mt-4 text-4xl font-medium">Have a Question?</p>
+                <div class="mt-8 grid grid-cols-2 gap-7">
+                    <input class="border py-1 px-3" placeholder="Name" />
+                    <input class="border py-1 px-3" placeholder="Email" />
+                    <input class="border py-1 px-3" placeholder="Subject" />
+                    <input class="border py-1 px-3" placeholder="Phone" />
+                    <textarea style="resize:none;" class="col-span-2 border h-40 px-3 py-1" placeholder="Message"></textarea>
+                    <button class="place-self-auto py-2 px-6 border border-green-400">Send</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
