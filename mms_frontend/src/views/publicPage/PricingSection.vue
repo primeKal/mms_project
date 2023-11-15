8e2f20e3feffb382b465eb15639691225800a1ca
Frezer <frezerzewdu@gmail.com>
2023-11-15 19:50:13 +0400

    css adjustment

--- mms_frontend/src/views/publicPage/PricingSection.vue
+++ mms_frontend/src/views/publicPage/PricingSection.vue
@@ -2,7 +2,7 @@
     <section class="py-16">
         <p class="text-center font-medium text-4xl">Choose A Plan<br/> That’s Right For You</p>
         <p class="mt-8 text-center text-black/60">Choose a plan that works best for you, feel free to contact us</p>
-        <div class="my-8  flex justify-center items-center ">
+        <div class="mt-8  flex justify-center items-center ">
             <div class="flex shadow-md rounded-md overflow-hidden">
                 <button 
                     @click="billPlan=0" 
@@ -21,39 +21,39 @@
             </div>
             
         </div>
-        <div class="flex justify-around">
-            <div :class="[selectedPlan == 1 ? 'pb-12 translate-y-10 bg-primary': 'pb-4 translate-y-0 bg-white']"  class="w-1/4 py-4 px-2 shadow rounded">
+        <div class="mt-14 flex justify-around">
+            <div class="w-1/5 py-4 px-5 shadow rounded-lg pb-4 bg-white">
                 <p class="text-xl text-center">Free</p>
                 <p class="my-4 text-black/60 text-center">Have a go and test your superpowers</p>
-                <p class="text-center text-xl font-semibold"><sup class="text-black/60">$</sup>0</p>
-                <div :class="[selectedPlan == 1 ? 'bg-white': 'bg-late-50']" class="mt-4 flex flex-col space-y-3">
+                <p class="text-center text-xl font-semibold"><sup class="text-black/60">$ </sup>0</p>
+                <div class="mt-4 py-4 px-2  flex flex-col space-y-5 bg-gray-50 rounded text-sm">
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         2 Users
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         2 Files
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         Public Share & Comments
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         Chat Support
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         New income apps
                     </div>
                 </div>                
             </div>
-            <div :class="[selectedPlan == 2 ? 'pb-12 translate-y-10 bg-primary': 'pb-4 translate-y-0 bg-white']"  class="w-1/4 py-4 px-2 shadow rounded">
-                <p class="text-xl text-center">Pro</p>
-                <p class="my-4 text-black/60 text-center">Experiment the power of infinite possibilities</p>
-                <p class="text-center text-xl font-semibold"><sup class="text-black/60">$</sup>8</p>
-                <div :class="[selectedPlan == 2 ? 'bg-white': 'bg-late-50']" class="mt-4 flex flex-col space-y-3">
+            <div class="w-1/5 py-4 px-5 shadow rounded-lg pb-16 -translate-y-6 bg-primary text-white">
+                <p class="text-2xl text-center font-semibold">Pro</p>
+                <p class="my-4 text-center">Experiment the power of infinite possibilities</p>
+                <p class="text-center text-xl font-semibold"><sup class="text-white/60">$</sup>8</p>
+                <div class="mt-4 py-4 px-2  flex flex-col space-y-5 bg-white text-black rounded">
                     <div class="flex items-center">
                         <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         4 Users
@@ -76,29 +76,29 @@
                     </div>
                 </div>                
             </div>
-            <div :class="[selectedPlan == 3 ? 'pb-12 translate-y-10 bg-primary': 'pb-4 translate-y-0 bg-white']"  class="w-1/4 py-4 px-2 shadow rounded">
+            <div class="w-1/5 py-4 px-5 shadow rounded-lg pb-4 bg-white">
                 <p class="text-xl text-center">Business</p>
                 <p class="my-4 text-black/60 text-center">Unveil new superpowers and join the Design Leaque</p>
                 <p class="text-center text-xl font-semibold"><sup class="text-black/60">$</sup>16</p>
-                <div :class="[selectedPlan == 3 ? 'bg-white': 'bg-late-50']" class="mt-4 flex flex-col space-y-3">
+                <div class="mt-4 py-4 px-2  flex flex-col space-y-5 bg-gray-50 rounded text-sm">
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         All the features of pro plan
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         Account success Manager
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         Single Sign-On (SSO)
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         Co-conception program
                     </div>
                     <div class="flex items-center">
-                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
+                        <svg class="mr-2 text-green-400" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9Z"/></svg>
                         Collaboration-Soon
                     </div>
                 </div>                
