6071a0375c49279572da3e5fe3d8382418fde305
Frezer <frezerzewdu@gmail.com>
2023-12-22 10:35:07 +0400

    chart components added

---
+++ mms_client/src/components/graphs/PerMonthSalesLineChart.vue
@@ -0,0 +1,20 @@
+<template>
+    <LineChart 
+        :chartData="graphData"
+    />
+</template>
+<script>
+import { defineComponent } from 'vue';
+import { LineChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+export default defineComponent({
+    name: 'PerMonthSalesChart',
+    props: {
+        graphData: Array,
+    },
+    components: { LineChart },
+})
+</script>
---
+++ mms_client/src/components/graphs/SalesBargraph.vue
@@ -0,0 +1,20 @@
+<template>
+    <BarChart 
+        :chartData="graphData"
+    />
+</template>
+<script>
+import { defineComponent } from 'vue';
+import { BarChart } from 'vue-chart-3'
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
+export default defineComponent({
+    name: 'SalesBarGraph',
+    props: {
+        graphData: Array,
+    },
+    components: { BarChart },
+})
+</script>
