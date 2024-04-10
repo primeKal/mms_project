064a6bb7cc963c7d0728b2c6d2ec0d53e1680618
Kaleb Teshale <kalebteshale72@gmail.com>
2024-04-10 22:36:07 +0300

    feature: added payment and payment method list view and route

---
+++ mms_client/src/views/MethodView/MethodList.vue
@@ -0,0 +1,36 @@
+<template>
+  <table class="w-full">
+    <thead class="bg-slate-50 border-y shadow-sm">
+      <th class="py-2 pl-4 text-left">Payment Method No.</th>
+      <th class="py-2 pl-4 text-left">Name</th>
+      <th class="py-2 pl-4 text-left">Transaction Count</th>
+      <th class="py-2 pl-4 text-center">Action</th>
+    </thead>
+    <tbody>
+      <tr
+        v-for="method in methods"
+        :key="method.id"
+        class="hover:bg-slate-50/60 border-b transition-colors"
+      >
+        <td class="pl-4 py-3 font-light">{{ method.id }}</td>
+        <td class="pl-4 py-3 font-light">{{ method.name }}</td>
+        <td class="pl-4 py-3 font-light">
+          {{ method.count }}
+        </td>
+        <td class="pl-4 py-3 flex justify-center items-center"></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  props: {
+    methods: Array,
+  },
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
