13b8f028076eb8d3ecb71a6b65e78b50a2cf2f08
ab <abrishzenebe7@gmail.com>
2024-04-11 15:24:55 +0300

    fix: indexing current company for fetching table and menus

--- mms_client/src/views/MenuMangementView/MenuManagementIndex.vue
+++ mms_client/src/views/MenuMangementView/MenuManagementIndex.vue
@@ -1,29 +1,17 @@
 <template>
     <MainRendererVue :loading="loading">
-        <QRCodeModalVue
-            v-if="openQRView"
-            :companyInfo="openQRView"
-            :qrValue="'https://www.google.com'"
-            @closeModal="()=> openQRView=null"
-        />
+        <QRCodeModalVue v-if="openQRView" :companyInfo="openQRView" :qrValue="'https://www.google.com'"
+            @closeModal="() => openQRView = null" />
 
-        <DeleteConfirmationModalVue 
-            v-if="openDelete"
-            :title="openDelete.name"
-            :id="openDelete.id"
-            @deleteItem="deleteMenu"
-            @closeModal="openDelete = null"
-        />
+        <DeleteConfirmationModalVue v-if="openDelete" :title="openDelete.name" :id="openDelete.id"
+            @deleteItem="deleteMenu" @closeModal="openDelete = null" />
 
         <div class="p-6 pr-12 w-full">
             <h3 class="text-primary font-medium text-xl">Menu List</h3>
             <div class="mt-4 w-full flex justify-between">
-            <SearchBarVue 
-                    class="w-2/3"
-                    :placeholderString="'Search menus'" 
-                    @searchChanged="searchCategories"
-            />
-                <button @click="openNewMenu = true" :disabled="openNewMenu" class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
+                <SearchBarVue class="w-2/3" :placeholderString="'Search menus'" @searchChanged="searchCategories" />
+                <button @click="openNewMenu = true" :disabled="openNewMenu"
+                    class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
                     + NEW MENU
                 </button>
             </div>
@@ -36,14 +24,9 @@
                 </thead>
                 <tbody>
                     <!-- create menu start -->
-                    <transition
-                        enter-from-class="h-0"
-                        enter-to-class="h-auto"
-                        enter-active-class="transition-all duration-500"
-                        leave-from-class="h-auto"
-                        leave-to-class="h-0"
-                        leave-active-class="transition-all duration-500"
-                    >
+                    <transition enter-from-class="h-0" enter-to-class="h-auto"
+                        enter-active-class="transition-all duration-500" leave-from-class="h-auto" leave-to-class="h-0"
+                        leave-active-class="transition-all duration-500">
                         <tr v-if="openNewMenu">
                             <td class="pl-4 py-1">New</td>
                             <td class="pl-4 py-2">
@@ -51,25 +34,40 @@
                             </td>
                             <td></td>
                             <td class="flex justify-center items-center space-x-3 pl-4 py-2">
-                                <button @click="openNewMenu = false" class="py-1 px-4 text-gray-400 hover:text-gray-500 active:scale-95 transition-all font-semibold rounded">cancel</button>
-                                <button @click="createMenu" class="py-1 px-4 text-white bg-green-400 hover:bg-green-500 active:scale-95 transition-all font-semibold rounded">Save</button>
-                                
+                                <button @click="openNewMenu = false"
+                                    class="py-1 px-4 text-gray-400 hover:text-gray-500 active:scale-95 transition-all font-semibold rounded">cancel</button>
+                                <button @click="createMenu"
+                                    class="py-1 px-4 text-white bg-green-400 hover:bg-green-500 active:scale-95 transition-all font-semibold rounded">Save</button>
+
                             </td>
                         </tr>
                     </transition>
                     <!-- create menu end -->
-                    <tr 
-                        v-for="menu in menus"
-                        :key="menu.id"
-                        class="hover:bg-slate-50/60 border-b transition-colors"
-                    >
+                    <tr v-for="menu in menus" :key="menu.id" class="hover:bg-slate-50/60 border-b transition-colors">
                         <td class="pl-4 py-3 font-light">{{ menu.id }}</td>
                         <td class="pl-4 py-3 font-light">{{ menu.name }}</td>
-                        <td :class="[menu.isActive ? 'text-green-400' : 'text-red-600']" class="pl-4 py-3 font-light">{{ menu.isActive }}</td>
+                        <td :class="[menu.isActive ? 'text-green-400' : 'text-red-600']" class="pl-4 py-3 font-light">{{
+                            menu.isActive }}</td>
                         <td class="pl-4 py-3 flex justify-center items-center">
-                            <svg @click="setQRinfo(menu)" class="p-1 hover:border rounded hover:text-blue-400 cursor-pointer transition-all" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5a2 2 0 0 0-2-2zm0 16H5V7h14v12zm-7-8.5c1.84 0 3.48.96 4.34 2.5c-.86 1.54-2.5 2.5-4.34 2.5s-3.48-.96-4.34-2.5c.86-1.54 2.5-2.5 4.34-2.5M12 9c-2.73 0-5.06 1.66-6 4c.94 2.34 3.27 4 6 4s5.06-1.66 6-4c-.94-2.34-3.27-4-6-4zm0 5.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5s1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z"/></svg>
-                            <svg @click="editMenu(menu)" class="p-1 hover:border rounded hover:text-yellow-400 cursor-pointer transition-all" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0L5 16ZM15 6l3 3m-5 11h8"/></svg>
-                            <svg @click="openDelete = menu" class="ml-3 p-1 hover:border rounded hover:text-red-500 cursor-pointer transition-all" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12M8 9h8v10H8V9m7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5Z"/></svg>
+                            <svg @click="setQRinfo(menu)"
+                                class="p-1 hover:border rounded hover:text-blue-400 cursor-pointer transition-all"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="currentColor"
+                                    d="M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5a2 2 0 0 0-2-2zm0 16H5V7h14v12zm-7-8.5c1.84 0 3.48.96 4.34 2.5c-.86 1.54-2.5 2.5-4.34 2.5s-3.48-.96-4.34-2.5c.86-1.54 2.5-2.5 4.34-2.5M12 9c-2.73 0-5.06 1.66-6 4c.94 2.34 3.27 4 6 4s5.06-1.66 6-4c-.94-2.34-3.27-4-6-4zm0 5.5c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5s1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z" />
+                            </svg>
+                            <svg @click="editMenu(menu)"
+                                class="p-1 hover:border rounded hover:text-yellow-400 cursor-pointer transition-all"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="m5 16l-1 4l4-1L19.586 7.414a2 2 0 0 0 0-2.828l-.172-.172a2 2 0 0 0-2.828 0L5 16ZM15 6l3 3m-5 11h8" />
+                            </svg>
+                            <svg @click="openDelete = menu"
+                                class="ml-3 p-1 hover:border rounded hover:text-red-500 cursor-pointer transition-all"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="currentColor"
+                                    d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12M8 9h8v10H8V9m7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5Z" />
+                            </svg>
                         </td>
                     </tr>
                 </tbody>
@@ -91,7 +89,7 @@ export default {
         QRCodeModalVue,
         DeleteConfirmationModalVue,
     },
-    data () {
+    data() {
         return {
             loading: true,
             newMenu: {
@@ -113,11 +111,14 @@ export default {
             fetchMenus: 'Menu/fetchAllMenus',
             removeMenu: 'Menu/removeMenu',
         }),
+        async initialization() {
+            await this.fetchMenus(this.basicInfo.id);
+        },
         searchCategories(searchString) {
             console.log(searchString)
             // do the search
         },
-        searchMenuItem (searchString) {
+        searchMenuItem(searchString) {
             console.log(searchString)
         },
         setQRinfo(menu) {
@@ -135,39 +136,39 @@ export default {
         },
         async createMenu() {
             this.loading = true
-            if(this.newMenu.name.length > 0) {
+            if (this.newMenu.name.length > 0) {
                 const status = await this.$store.dispatch('Menu/addNewMenu', this.newMenu)
-                if(status.success){
+                if (status.success) {
                     this.$toast.success('New Menu created')
                     this.openNewMenu = false
                     this.newMenu.name = ''
-                }else {
+                } else {
                     this.$toast.error('Error creating menu')
                 }
             }
-            setTimeout(()=>{
+            setTimeout(() => {
                 this.loading = false
-            },1000)
-            
+            }, 1000)
+
         },
-        editMenu (menu){
+        editMenu(menu) {
             this.$store.commit('Menu/setMenuId', menu.id)
-            this.$router.push({name: 'edit_menu', params: {name: menu.name}})
+            this.$router.push({ name: 'edit_menu', params: { name: menu.name } })
         },
         async deleteMenu(id) {
             const status = await this.removeMenu(id)
-            if(status.success) {
+            if (status.success) {
                 this.$toast.success('Menu deleted successfully')
                 this.openDelete = null
-            }else {
+            } else {
                 this.$toast.error('Error occured')
             }
         }
     },
-    async mounted () {
+    async mounted() {
         this.$emit('selectedNav', 3)
-        await this.fetchMenus(1)
         this.loading = false
+        this.initialization()
     }
 }
 </script>
--- mms_client/src/views/TableManagementView/TableManagementView.vue
+++ mms_client/src/views/TableManagementView/TableManagementView.vue
@@ -1,22 +1,14 @@
 <template>
     <MainRendererVue :loading="loading">
         <div class="p-6 pr-12 w-full">
-            <AddTableViewVue 
-                class="z-10" 
-                v-if="addTable || editTable"
-                :editTable="editTable"
-                :isNew="editTable ? false : true" 
-                @closeModal="addTable=false, editTable=null" 
-            />
+            <AddTableViewVue class="z-10" v-if="addTable || editTable" :editTable="editTable"
+                :isNew="editTable ? false : true" @closeModal="addTable = false, editTable = null" />
 
             <h3 class="text-primary font-medium text-xl">Table</h3>
             <div class="mt-8 w-full flex justify-between">
                 <div class="flex justify-between w-3/5">
-                    <SearchBarVue 
-                        class="w-3/5"
-                        :placeholderString="'Search by table number'"
-                        @searchChanged="searchTables"
-                    />
+                    <SearchBarVue class="w-3/5" :placeholderString="'Search by table number'"
+                        @searchChanged="searchTables" />
                     <fieldset class="-mt-1 w-1/3 border rounded">
                         <legend class="ml-3 text-xs px-1">Table Status</legend>
                         <select class="w-full pt-1 pb-1 px-2 outline-none">
@@ -27,25 +19,23 @@
                     </fieldset>
 
                 </div>
-            <button @click="addTable=true" class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
+                <button @click="addTable = true"
+                    class="w-1/5 bg-transparent hover:bg-primary text-primary hover:text-white border-primary  rounded border active:scale-95 transition-all">
                     + NEW TABLE
                 </button>
             </div>
-            <TableListVue 
-                class="mt-5"
-                :tables="tables"
-                @editTable="(table)=>{editTable = table}"
-            />
+            <TableListVue class="mt-5" :tables="tables" @editTable="(table) => { editTable = table }" />
         </div>
-    </MainRendererVue>        
+    </MainRendererVue>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
 import MainRendererVue from '@/layout/MainRenderer.vue';
 import AddTableViewVue from './AddTableView.vue'
 import SearchBarVue from '@/components/fields/SearchBar.vue';
 import TableListVue from './TablesList.vue'
+
 export default {
     components: {
         MainRendererVue,
@@ -53,7 +43,7 @@ export default {
         SearchBarVue,
         TableListVue,
     },
-    data () {
+    data() {
         return {
             loading: true,
             addTable: false,
@@ -62,18 +52,29 @@ export default {
         }
     },
     computed: {
+
         ...mapGetters({
-            tables: 'Table/getTables'
-        })
+            tables: 'Table/getTables',
+            basicInfo: 'Company/getCompanyInfo',
+
+        }),
     },
     methods: {
+        ...mapActions({
+            fetchTables: 'Table/fetchTables',
+        }),
+
+        async initialization() {
+            await this.fetchTables(this.basicInfo.id)
+        },
+
         searchTables(searchString) {
             console.log(searchString)
         },
     },
-    async mounted () {
+    async mounted() {
         this.$emit('selectedNav', 4)
-        await this.$store.dispatch('Table/fetchTables')
+        this.initialization();
         this.loading = false
     }
 }
