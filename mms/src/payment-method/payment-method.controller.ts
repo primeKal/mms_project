8d6fe77c379c170bac13f0ddd663429e63a51fa9
Kaleb Teshale <kalebteshale72@gmail.com>
2024-04-23 15:55:38 +0300

    bug fix

--- mms/src/payment-method/payment-method.controller.ts
+++ mms/src/payment-method/payment-method.controller.ts
@@ -6,47 +6,52 @@ import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwtAuthGuard';
 import { UpdateStatusDto } from 'src/utils/dtos/updateStatusDto';
 
-@UseGuards(JwtAuthGuard)
 @ApiTags('PaymentMethod')
 @Controller('paymentMethod')
 export class PaymentMethodController {
-    constructor(private readonly paymentMethodService: PaymentMethodService){
+  constructor(private readonly paymentMethodService: PaymentMethodService) {
 
-    }
-    @Get()
-    public async getPaymentMethod(@Query('page') page: number = 1, @Query('pageSize') pageSize: number = 10): Promise<PaymentMethodModel[]> {
-      return this.paymentMethodService.getAllPaymentMethods(page,pageSize);
-    }
+  }
 
-    // @UseGuards(JwtAuthGuard)
-    @Get(':id')
-    public async getAPaymentMethod(@Param('id') id: number): Promise<PaymentMethodModel>{
-      return this.paymentMethodService.getOnePaymentMethodById(id);
-    }
+  @Get()
+  public async getPaymentMethod(@Query('page') page: number = 1, @Query('pageSize') pageSize: number = 10): Promise<PaymentMethodModel[]> {
+    return this.paymentMethodService.getAllPaymentMethods(page, pageSize);
+  }
 
-    @Get('ByCompany/:id')
-    public async getPaymentMethodsByCompany(@Param('id') id: number): Promise<[PaymentMethodModel]>{
-      return this.paymentMethodService.getPaymentMethodsByCompany(id);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  public async getAPaymentMethod(@Param('id') id: number): Promise<PaymentMethodModel> {
+    return this.paymentMethodService.getOnePaymentMethodById(id);
+  }
 
-    @Post()
-    public async createPaymentMethod(@Req() req: any, @Body() body: PaymentMethodDto): Promise<any> {
-      return this.paymentMethodService.createPaymentMethod(req.user.id,body);
-    }
 
-    @Put()
-    public async editPaymentMethod(@Body() body: PaymentMethodDto): Promise<PaymentMethodModel> {
+  @Get('ByCompany/:id')
+  public async getPaymentMethodsByCompany(@Param('id') id: number): Promise<[PaymentMethodModel]> {
+    return this.paymentMethodService.getPaymentMethodsByCompany(id);
+  }
 
-      return this.paymentMethodService.editPaymentMethod(body);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  public async createPaymentMethod(@Req() req: any, @Body() body: PaymentMethodDto): Promise<any> {
+    return this.paymentMethodService.createPaymentMethod(req.user.id, body);
+  }
 
-    @Delete()
-    public async deletePaymentMethod(@Body() id): Promise<void>{
-      return this.paymentMethodService.deletePaymentMethod(id.id);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Put()
+  public async editPaymentMethod(@Body() body: PaymentMethodDto): Promise<PaymentMethodModel> {
 
-    @Post("UpdateStatus")
-    public async updateStatus(@Req() req: any,@Body() body: UpdateStatusDto): Promise<PaymentMethodModel> {
-      return this.paymentMethodService.updateOrderStatus(body);
-    }
+    return this.paymentMethodService.editPaymentMethod(body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete()
+  public async deletePaymentMethod(@Body() id): Promise<void> {
+    return this.paymentMethodService.deletePaymentMethod(id.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post("UpdateStatus")
+  public async updateStatus(@Req() req: any, @Body() body: UpdateStatusDto): Promise<PaymentMethodModel> {
+    return this.paymentMethodService.updateOrderStatus(body);
+  }
 }
